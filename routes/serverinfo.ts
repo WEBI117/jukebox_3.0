9b0ab2efb0ffb6d44e99697602c949176b41742b
Wahid <wejaz@ssidecisions.com>
2023-01-11 19:20:28 +0500

    serverinfo untested fix

--- routes/serverinfo.ts
+++ routes/serverinfo.ts
@@ -3,29 +3,46 @@ const router = express.Router();
 import tokenhelper from '../helpers/tokenhelper';
 const { networkInterfaces } = require('os');
 
+const getIPInfoFromObject = (netsObject: any) => {
+    var keys = Object.keys(netsObject)
+    if (keys) {
+
+    }
+    else {
+
+    }
+    return netsObject[keys[0]]
+}
 
 router.get('/', (req, res) => {
-    const nets = networkInterfaces();
-    const results = Object.create(null); // Or just '{}', an empty object
+    try {
+        const nets = networkInterfaces();
+        const results = Object.create(null); // Or just '{}', an empty object
 
-    for (const name of Object.keys(nets)) {
-        for (const net of nets[name]) {
-            // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
-            // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
-            const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
-            if (net.family === familyV4Value && !net.internal) {
-                if (!results[name]) {
-                    results[name] = [];
+        for (const name of Object.keys(nets)) {
+            for (const net of nets[name]) {
+                // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+                // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
+                const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
+                if (net.family === familyV4Value && !net.internal) {
+                    if (!results[name]) {
+                        results[name] = [];
+                    }
+                    results[name].push(net.address);
                 }
-                results[name].push(net.address);
             }
         }
+        console.log(results)
+        if (results) {
+            res.send(getIPInfoFromObject(results))
+            return
+        }
+        else {
+            res.send('')
+        }
     }
-    console.log(results)
-    if(results.en0){
-        res.send(results.en0[0]) 
-    }
-    else{
+    catch (err) {
+        console.log(err)
         res.send('')
     }
 })
